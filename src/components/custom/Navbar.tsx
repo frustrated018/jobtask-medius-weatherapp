8bea224cb5de3116a51d9eff207f0272aeded426
frustrated018 <niloyd018@gmail.com>
2024-05-15 20:50:20 +0600

    Added dark mode

--- src/components/custom/Navbar.tsx
+++ src/components/custom/Navbar.tsx
@@ -1,11 +1,13 @@
-import { Button } from "@/components/ui/button"
+import { Button } from "@/components/ui/button";
+import { ThemeToggle } from "../theme-toggle";
 
 const Navbar = () => {
   return (
     <div>
       <Button>Hellloooo</Button>
+      <ThemeToggle />
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
