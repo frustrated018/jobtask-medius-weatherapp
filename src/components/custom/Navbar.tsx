59f8a889931fd9944aba80e546332749ff3228f5
frustrated018 <niloyd018@gmail.com>
2024-05-15 21:05:10 +0600

    Created basic navbar

--- src/components/custom/Navbar.tsx
+++ src/components/custom/Navbar.tsx
@@ -1,12 +1,42 @@
-import { Button } from "@/components/ui/button";
+import { Button, buttonVariants } from "@/components/ui/button";
 import { ThemeToggle } from "../theme-toggle";
+import { Input } from "@/components/ui/input";
+
+import Link from "next/link";
 
 const Navbar = () => {
   return (
-    <div>
-      <Button>Hellloooo</Button>
-      <ThemeToggle />
-    </div>
+    <nav className="py-5 px-2.5 flex justify-between items-center w-full border-b-2">
+      <Link href={"/"} className="text-lg xl:text-2xl font-bold">
+        Medius Weather
+      </Link>
+
+      <div className="flex items-center gap-2">
+        <Link href={"/"} className={buttonVariants({ variant: "link" })}>
+          Home
+        </Link>
+        <Link href={"/about"} className={buttonVariants({ variant: "link" })}>
+          About
+        </Link>
+        <Link
+          href={"/services"}
+          className={buttonVariants({ variant: "link" })}
+        >
+          Services
+        </Link>
+        <Link href={"/contact"} className={buttonVariants({ variant: "link" })}>
+          Contact
+        </Link>
+      </div>
+
+      <div className="flex items-center gap-3">
+        <div className="flex w-full max-w-sm items-center space-x-2">
+          <Input type="text" placeholder="City" />
+          <Button type="submit">Search</Button>
+        </div>
+        <ThemeToggle />
+      </div>
+    </nav>
   );
 };
 
